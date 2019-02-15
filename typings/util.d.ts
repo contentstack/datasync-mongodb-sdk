bc49c9502d0e84eda35f2fd77876da4f9fe98d15
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-02-15 13:44:25 +0530

    test cases updated, 80% coverage achieved + deprecation warning issue resolved

--- typings/util.d.ts
+++ typings/util.d.ts
@@ -3,7 +3,5 @@
  * Copyright (c) 2019 Contentstack LLC
  * MIT Licensed
  */
-export declare const append: (field: any) => string;
 export declare const validateURI: (uri: any) => string;
 export declare const checkCyclic: (uid: any, mapping: any) => boolean;
-export declare const mask: (json: any, filter: any) => void;
