201407bb94f826cfd5645ec3145e91dd0e9054b7
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-02-27 22:56:34 +0530

    backup

--- dist/config.js
+++ dist/config.js
@@ -6,28 +6,30 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
-    collectionName: 'contents',
-    dbName: 'contentstack-persistent-db',
-    limit: 100,
-    locales: [],
-    options: {
-        autoReconnect: true,
-        connectTimeoutMS: 15000,
-        keepAlive: true,
-        noDelay: true,
-        reconnectInterval: 1000,
-        reconnectTries: 20,
-        useNewUrlParser: true,
-    },
-    projections: {
-        _id: 0,
-        _version: 0,
-        content_type_uid: 0,
-        created_at: 0,
-        sys_keys: 0,
-        updated_at: 0,
-        updated_by: 0,
-    },
-    skip: 0,
-    uri: 'mongodb://localhost:27017',
+    contentStore: {
+        collectionName: 'contents',
+        dbName: 'contentstack-persistent-db',
+        limit: 100,
+        locales: [],
+        options: {
+            autoReconnect: true,
+            connectTimeoutMS: 15000,
+            keepAlive: true,
+            noDelay: true,
+            reconnectInterval: 1000,
+            reconnectTries: 20,
+            useNewUrlParser: true,
+        },
+        projections: {
+            _id: 0,
+            _version: 0,
+            content_type_uid: 0,
+            created_at: 0,
+            sys_keys: 0,
+            updated_at: 0,
+            updated_by: 0,
+        },
+        skip: 0,
+        uri: 'mongodb://localhost:27017',
+    }
 };
