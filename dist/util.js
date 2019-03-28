d03c74e8685289973395f4bbcd6b3bf27874f860
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-03-28 12:10:45 +0530

    README, Docs and method comments updated

--- dist/util.js
+++ dist/util.js
@@ -7,10 +7,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const lodash_1 = require("lodash");
 /**
- * @summary
- *  Validates the mongodb 'uri' passed
- * @param {String} uri - Mongodb connection 'uri' string
- * @returns {String} - Returns the `uri` after validating it, else throws an error
+ * @private
+ *
+ * @description
+ * Validates the mongodb 'uri' passed
+ * @param {string} uri - Mongodb connection 'uri' string
+ * @returns {string} - Returns the `uri` after validating it, else throws an error
  */
 exports.validateURI = (uri) => {
     if (typeof uri !== 'string' || uri.length === 0) {
@@ -19,11 +21,12 @@ exports.validateURI = (uri) => {
     return uri;
 };
 /**
- * @summary
- *  Checks for `cyclic` references
- * @param {String} uid - Uid to check if it exists on `map`
- * @param {Object} mapping - Map of the uids tracked thusfar
- * @returns {Boolean} - Returns `true` if the `uid` is part of the map (i.e. cyclic)
+ * @private
+ * @method checkCyclic
+ * @summary Checks for `cyclic` references
+ * @param {string} uid Uid to check if it exists on `map`
+ * @param {object} mapping Map of the uids tracked thusfar
+ * @returns {boolean} Returns `true` if the `uid` is part of the map (i.e. cyclic)
  */
 exports.checkCyclic = (uid, mapping) => {
     let flag = false;
